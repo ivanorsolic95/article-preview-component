e902343e7adb095e8f7052d734f7217460fc3efc
Ivan Orsolic <orsolic1995@gmail.com>
2024-07-10 10:01:38 +0200

    Fix problem with toggling on smaller screens
    Event listener was missing on one of the toggle buttons.

--- toggle.js
+++ toggle.js
@@ -1,11 +1,12 @@
 const widerScreen = window.matchMedia("(max-width: 87.5rem)");
 
 const shareButton = document.getElementById("share-button");
+const shareLinksButton = document.getElementById("share-links-button");
 const showDiv = document.getElementById("show");
 const hideDiv = document.getElementById("hide");
 const textBubble = document.getElementById("text-bubble");
 
-shareButton.addEventListener('click', function() {
+shareLinksButton.addEventListener('click', function() {
     if (widerScreen.matches) {
         if (hideDiv.style.display === 'none' || hideDiv.style.display === '') {
             showDiv.style.display = 'none';
@@ -22,6 +23,15 @@ shareButton.addEventListener('click', function() {
         }
     }
 });
+
+shareButton.addEventListener('click', function() {
+    if(widerScreen.matches) {
+        if (showDiv.style.display === 'none' || hideDiv.style.display === '') {
+            hideDiv.style.display = 'none';
+            showDiv.style.display = 'flex';
+        }
+    }
+})
 
 // Ensure initial state
 if (widerScreen.matches) {
